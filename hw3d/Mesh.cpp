14f83314b4f4892943e1e1eb9bee311b949e1cf3
aleurankar <aleurankar@DEV-MACHINE>
2025-06-17 20:25:33 +0200

    specular is weird on this boi

--- hw3d/Mesh.cpp
+++ hw3d/Mesh.cpp
@@ -241,14 +241,14 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 {
 	using Dvtx::VertexLayout;
 
-	Dvtx::VertexBuffer vbuf( std::move(
+	Dvtx::VertexBuffer vbuf(std::move(
 		VertexLayout{}
-		.Append( VertexLayout::Position3D )
-		.Append( VertexLayout::Normal )
+		.Append(VertexLayout::Position3D)
+		.Append(VertexLayout::Normal)
 		.Append(VertexLayout::Texture2D)
-	) );
+	));
 
-	for( unsigned int i = 0; i < mesh.mNumVertices; i++ )
+	for (unsigned int i = 0; i < mesh.mNumVertices; i++)
 	{
 		vbuf.EmplaceBack(
 			*reinterpret_cast<dx::XMFLOAT3*>(&mesh.mVertices[i]),
@@ -258,18 +258,19 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 	}
 
 	std::vector<unsigned short> indices;
-	indices.reserve( mesh.mNumFaces * 3 );
-	for( unsigned int i = 0; i < mesh.mNumFaces; i++ )
+	indices.reserve(mesh.mNumFaces * 3);
+	for (unsigned int i = 0; i < mesh.mNumFaces; i++)
 	{
 		const auto& face = mesh.mFaces[i];
-		assert( face.mNumIndices == 3 );
-		indices.push_back( face.mIndices[0] );
-		indices.push_back( face.mIndices[1] );
-		indices.push_back( face.mIndices[2] );
+		assert(face.mNumIndices == 3);
+		indices.push_back(face.mIndices[0]);
+		indices.push_back(face.mIndices[1]);
+		indices.push_back(face.mIndices[2]);
 	}
 
 	std::vector<std::unique_ptr<Bind::Bindable>> bindablePtrs;
 
+	bool hasSpecularMap = false;
 	if (mesh.mMaterialIndex >= 0)
 	{
 		auto& material = *pMaterials[mesh.mMaterialIndex];
@@ -278,59 +279,67 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		const auto base = "Models\\nano_textured\\"s;
 		aiString texFileName;
 
+		material.GetTexture(aiTextureType_DIFFUSE, 0, &texFileName);
+		bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile(base + texFileName.C_Str())));
+
 		if (material.GetTexture(aiTextureType_SPECULAR, 0, &texFileName) == aiReturn_SUCCESS)
 		{
-			bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx,Surface::FromFile(base + texFileName.C_Str()),1));
+			bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile(base + texFileName.C_Str()), 1));
+			hasSpecularMap = true;
 		}
-
-		material.GetTexture(aiTextureType_SPECULAR,0,&texFileName);
-		bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx,Surface::FromFile(base + texFileName.C_Str()),1));
 
 		bindablePtrs.push_back(std::make_unique<Bind::Sampler>(gfx));
 	}
 
-	bindablePtrs.push_back( std::make_unique<Bind::VertexBuffer>( gfx,vbuf ) );
+	bindablePtrs.push_back(std::make_unique<Bind::VertexBuffer>(gfx, vbuf));
 
-	bindablePtrs.push_back( std::make_unique<Bind::IndexBuffer>( gfx,indices ) );
+	bindablePtrs.push_back(std::make_unique<Bind::IndexBuffer>(gfx, indices));
 
-	auto pvs = std::make_unique<Bind::VertexShader>( gfx,L"PhongVS.cso" );
+	auto pvs = std::make_unique<Bind::VertexShader>(gfx, L"PhongVS.cso");
 	auto pvsbc = pvs->GetBytecode();
-	bindablePtrs.push_back( std::move( pvs ) );
+	bindablePtrs.push_back(std::move(pvs));
 
-	bindablePtrs.push_back( std::make_unique<Bind::PixelShader>( gfx,L"PhongPS.cso" ) );
+	bindablePtrs.push_back(std::make_unique<Bind::InputLayout>(gfx, vbuf.GetLayout().GetD3DLayout(), pvsbc));
 
-	bindablePtrs.push_back( std::make_unique<Bind::InputLayout>( gfx,vbuf.GetLayout().GetD3DLayout(),pvsbc ) );
-
-	struct PSMaterialConstant
+	if (hasSpecularMap)
 	{
-		float specularIntensity = 0.6f;
-		float specularPower = 30.0f;
-		float padding[2];
-	} pmc;
-	bindablePtrs.push_back( std::make_unique<Bind::PixelConstantBuffer<PSMaterialConstant>>( gfx,pmc,1u ) );
-
-	return std::make_unique<Mesh>( gfx,std::move( bindablePtrs ) );
-}
-
-std::unique_ptr<Node> Model::ParseNode( int& nextId,const aiNode& node ) noexcept
-{
-	namespace dx = DirectX;
-	const auto transform = dx::XMMatrixTranspose( dx::XMLoadFloat4x4(
-		reinterpret_cast<const dx::XMFLOAT4X4*>(&node.mTransformation)
-	) );
-
-	std::vector<Mesh*> curMeshPtrs;
-	curMeshPtrs.reserve( node.mNumMeshes );
-	for( size_t i = 0; i < node.mNumMeshes; i++ )
+		bindablePtrs.push_back(std::make_unique<Bind::PixelShader>(gfx, L"PhongPSSpecMap.cso"));
+	}
+	else
 	{
-		const auto meshIdx = node.mMeshes[i];
-		curMeshPtrs.push_back( meshPtrs.at( meshIdx ).get() );
+		bindablePtrs.push_back(std::make_unique<Bind::PixelShader>(gfx, L"PhongPS.cso"));
+
+		struct PSMaterialConstant
+		{
+			float specularIntensity = 1.6f;
+			float specularPower = 50.0f;
+			float padding[2];
+		} pmc;
+		bindablePtrs.push_back(std::make_unique<Bind::PixelConstantBuffer<PSMaterialConstant>>(gfx, pmc, 1u));
 	}
 
-	auto pNode = std::make_unique<Node>( nextId++,node.mName.C_Str(),std::move( curMeshPtrs ),transform );
-	for( size_t i = 0; i < node.mNumChildren; i++ )
+	return std::make_unique<Mesh>(gfx, std::move(bindablePtrs));
+}
+
+std::unique_ptr<Node> Model::ParseNode(int& nextId, const aiNode& node) noexcept
+{
+	namespace dx = DirectX;
+	const auto transform = dx::XMMatrixTranspose(dx::XMLoadFloat4x4(
+		reinterpret_cast<const dx::XMFLOAT4X4*>(&node.mTransformation)
+	));
+
+	std::vector<Mesh*> curMeshPtrs;
+	curMeshPtrs.reserve(node.mNumMeshes);
+	for (size_t i = 0; i < node.mNumMeshes; i++)
 	{
-		pNode->AddChild( ParseNode( nextId,*node.mChildren[i] ) );
+		const auto meshIdx = node.mMeshes[i];
+		curMeshPtrs.push_back(meshPtrs.at(meshIdx).get());
+	}
+
+	auto pNode = std::make_unique<Node>(nextId++, node.mName.C_Str(), std::move(curMeshPtrs), transform);
+	for (size_t i = 0; i < node.mNumChildren; i++)
+	{
+		pNode->AddChild(ParseNode(nextId, *node.mChildren[i]));
 	}
 
 	return pNode;
