61587bf57eb1a706a8ede1d51bf043e017e72215
ales2026 <ale.urankar@gmail.com>
2025-11-18 10:33:08 +0100

    pull normal mapping out of main shader

--- hw3d/PhongPSNormalMapObject.hlsl
+++ hw3d/PhongPSNormalMapObject.hlsl
@@ -11,37 +11,41 @@ cbuffer LightCBuf
 
 cbuffer ObjectCBuf
 {
-    float specularIntensity;
-    float specularPower;
     bool normalMapEnabled;
-    float padding[1];
-};
-
-cbuffer TransformCBuf
-{
-    matrix modelView;
-    matrix modelViewProj;
+    bool specularMapEnabled;
+    bool hasGloss;
+    float specularPowerConst;
+    float3 specularColor;
+    float specularMapWeight;
 };
 
 Texture2D tex;
-Texture2D nmap : register(t2);
+Texture2D spec;
+Texture2D nmap;
 
 SamplerState splr;
 
-
-float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float2 tc : Texcoord) : SV_Target
+float3 MapNormalViewSpace(const float3 tan, const float3 bitan, const float3 viewNormal, const float2 tc, Texture2D nmap, SamplerState splr)
 {
-	// sample normal from map if normal mapping enabled
+    // build the tranform (rotation) into tangent space
+    const float3x3 tanToView = float3x3(
+        normalize(tan),
+        normalize(bitan),
+        normalize(viewNormal)
+    );
+    // sample and unpack the normal from texture into tangent space   
+    const float3 normalSample = nmap.Sample(splr, tc).xyz;
+    const float3 tanNormal = normalSample * 2.0f - 1.0f;
+    // bring normal from tanspace into view space
+    return normalize(mul(tanNormal, tanToView));
+}
+
+float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tan : Tangent, float3 bitan : Bitangent, float2 tc : Texcoord) : SV_Target
+{
+    // sample normal from map if normal mapping enabled
     if (normalMapEnabled)
     {
-        // sanple and unpack normal data
-        const float3 normalSample = nmap.Sample(splr, tc).xyz;
-        float3 tanNormal;
-        tanNormal.x = normalSample.x * 2.0f - 1.0f;
-        tanNormal.y = -normalSample.y * 2.0f + 1.0f;
-        tanNormal.z = -normalSample.z * 2.0f + 1.0f;
-        // bring normal from object space into view space
-        viewNormal = normalize(mul(tanNormal, (float3x3) modelView));
+        viewNormal = MapNormalViewSpace(tan, bitan, viewNormal, tc, nmap, splr);
     }
 	// fragment to light vector data
     const float3 vToL = lightPos - viewPos;
@@ -55,7 +59,22 @@ float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float2 tc : T
     const float3 w = viewNormal * dot(vToL, viewNormal);
     const float3 r = w * 2.0f - vToL;
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-    const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
+    float3 specularReflectionColor;
+    float specularPower = specularPowerConst;
+    if (specularMapEnabled)
+    {
+        const float4 specularSample = spec.Sample(splr, tc);
+        specularReflectionColor = specularSample.rgb * specularMapWeight;
+        if (hasGloss)
+        {
+            specularPower = pow(2.0f, specularSample.a * 13.0f);
+        }
+    }
+    else
+    {
+        specularReflectionColor = specularColor;
+    }
+    const float3 specular = att * (diffuseColor * diffuseIntensity) * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
 	// final color
-    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular), 1.0f);
+    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular * specularReflectionColor), 1.0f);
 }
