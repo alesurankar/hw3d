dae3c6fc4cdcdf5ce1b6748002dca98cd1902f84
ales2026 <ale.urankar@gmail.com>
2025-11-20 16:45:39 +0100

    basic job technique / job system in model + enable gobbler
    dcbuf alignment issues fixed

--- hw3d/Node.cpp
+++ hw3d/Node.cpp
@@ -36,33 +36,33 @@ void Node::AddChild(std::unique_ptr<Node> pChild) noxnd
 	childPtrs.push_back(std::move(pChild));
 }
 
-void Node::ShowTree(Node*& pSelectedNode) const noexcept
-{
-	// if there is no selected node, set selectedId to an impossible value
-	const int selectedId = (pSelectedNode == nullptr) ? -1 : pSelectedNode->GetId();
-	// build up flags for current node
-	const auto node_flags = ImGuiTreeNodeFlags_OpenOnArrow
-		| ((GetId() == selectedId) ? ImGuiTreeNodeFlags_Selected : 0)
-		| ((childPtrs.size() == 0) ? ImGuiTreeNodeFlags_Leaf : 0);
-	// render this node
-	const auto expanded = ImGui::TreeNodeEx(
-		(void*)(intptr_t)GetId(), node_flags, name.c_str()
-	);
-	// processing for selecting node
-	if (ImGui::IsItemClicked())
-	{
-		pSelectedNode = const_cast<Node*>(this);
-	}
-	// recursive rendering of open node's children
-	if (expanded)
-	{
-		for (const auto& pChild : childPtrs)
-		{
-			pChild->ShowTree(pSelectedNode);
-		}
-		ImGui::TreePop();
-	}
-}
+//void Node::ShowTree(Node*& pSelectedNode) const noexcept
+//{
+//	// if there is no selected node, set selectedId to an impossible value
+//	const int selectedId = (pSelectedNode == nullptr) ? -1 : pSelectedNode->GetId();
+//	// build up flags for current node
+//	const auto node_flags = ImGuiTreeNodeFlags_OpenOnArrow
+//		| ((GetId() == selectedId) ? ImGuiTreeNodeFlags_Selected : 0)
+//		| ((childPtrs.size() == 0) ? ImGuiTreeNodeFlags_Leaf : 0);
+//	// render this node
+//	const auto expanded = ImGui::TreeNodeEx(
+//		(void*)(intptr_t)GetId(), node_flags, name.c_str()
+//	);
+//	// processing for selecting node
+//	if (ImGui::IsItemClicked())
+//	{
+//		pSelectedNode = const_cast<Node*>(this);
+//	}
+//	// recursive rendering of open node's children
+//	if (expanded)
+//	{
+//		for (const auto& pChild : childPtrs)
+//		{
+//			pChild->ShowTree(pSelectedNode);
+//		}
+//		ImGui::TreePop();
+//	}
+//}
 
 void Node::SetAppliedTransform(DirectX::FXMMATRIX transform) noexcept
 {
