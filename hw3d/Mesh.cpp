1d804b643dc2b2e5d0ecf9ea51180e0bb25aae69
ales2026 <ale.urankar@gmail.com>
2025-11-20 10:53:19 +0100

    outline in sponza and zbuffer whatsies

--- hw3d/Mesh.cpp
+++ hw3d/Mesh.cpp
@@ -8,6 +8,7 @@
 #include "DynamicConstant.h"
 #include "ConstantBuffersEx.h"
 #include "LayoutCodex.h"
+#include "Stencil.h"
 
 namespace dx = DirectX;
 
@@ -727,7 +728,7 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 	// of signalling 2-sidedness to be more general in the future
 	bindablePtrs.push_back(Rasterizer::Resolve(gfx, hasAlphaDiffuse));
 
-	bindablePtrs.push_back(Blender::Resolve(gfx, false));
+	bindablePtrs.push_back(std::make_shared<Stencil>(gfx, Stencil::Mode::Off));
 
 	return std::make_unique<Mesh>(gfx, std::move(bindablePtrs));
 }
