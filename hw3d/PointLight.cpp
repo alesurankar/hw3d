3e079a07c8c02049fbb6858f3e9e0901b8f72fbf
ales2026 <ale.urankar@gmail.com>
2025-11-21 06:38:20 +0100

    sponza reinstated

--- hw3d/PointLight.cpp
+++ hw3d/PointLight.cpp
@@ -40,8 +40,8 @@ void PointLight::SpawnControlWindow() noexcept
 void PointLight::Reset() noexcept
 {
 	cbData = {
-		{ 1.0f,2.5f,-8.0f },
-		{ 0.05f,0.05f,0.05f },
+		{ 10.0f,9.0f,2.5f },
+		{ 0.2f,0.2f,0.2f },
 		{ 1.0f,1.0f,1.0f },
 		1.0f,
 		1.0f,
