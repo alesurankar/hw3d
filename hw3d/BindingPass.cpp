fe0a1778b98b498f3b187e139d86a509c6c764ee
ales2026 <ale.urankar@gmail.com>
2026-01-06 09:12:55 +0100

    Rgph namespace

--- hw3d/BindingPass.cpp
+++ hw3d/BindingPass.cpp
@@ -4,32 +4,35 @@
 #include "DepthStencil.h"
 #include "RenderGraphCompileException.h"
 
-BindingPass::BindingPass(std::string name, std::vector<std::shared_ptr<Bind::Bindable>> binds)
-	:
-	Pass(std::move(name)),
-	binds(std::move(binds))
+namespace Rgph
 {
-}
-
-void BindingPass::AddBind(std::shared_ptr<Bind::Bindable> bind) noexcept
-{
-	binds.push_back(std::move(bind));
-}
-
-void BindingPass::BindAll(Graphics& gfx) const noexcept
-{
-	for (auto& bind : binds)
+	BindingPass::BindingPass(std::string name, std::vector<std::shared_ptr<Bind::Bindable>> binds)
+		:
+		Pass(std::move(name)),
+		binds(std::move(binds))
 	{
-		bind->Bind(gfx);
 	}
-	BindBufferResources(gfx);
-}
 
-void BindingPass::Finalize()
-{
-	Pass::Finalize();
-	if (!renderTarget && !depthStencil)
+	void BindingPass::AddBind(std::shared_ptr<Bind::Bindable> bind) noexcept
 	{
-		throw RGC_EXCEPTION("BindingPass [" + GetName() + "] needs at least one of a renderTarget or depthStencil");
+		binds.push_back(std::move(bind));
+	}
+
+	void BindingPass::BindAll(Graphics& gfx) const noexcept
+	{
+		for (auto& bind : binds)
+		{
+			bind->Bind(gfx);
+		}
+		BindBufferResources(gfx);
+	}
+
+	void BindingPass::Finalize()
+	{
+		Pass::Finalize();
+		if (!renderTarget && !depthStencil)
+		{
+			throw RGC_EXCEPTION("BindingPass [" + GetName() + "] needs at least one of a renderTarget or depthStencil");
+		}
 	}
 }
--- hw3d/Job.cpp
+++ hw3d/Job.cpp
@@ -2,17 +2,19 @@
 #include "Step.h"
 #include "Drawable.h"
 
-
-Job::Job(const Step* pStep, const Drawable* pDrawable)
-	:
-	pDrawable{ pDrawable },
-	pStep{ pStep }
+namespace Rgph
 {
-}
+	Job::Job(const Step* pStep, const Drawable* pDrawable)
+		:
+		pDrawable{ pDrawable },
+		pStep{ pStep }
+	{
+	}
 
-void Job::Execute(Graphics& gfx) const noxnd
-{
-	pDrawable->Bind(gfx);
-	pStep->Bind(gfx);
-	gfx.DrawIndexed(pDrawable->GetIndexCount());
+	void Job::Execute(Graphics& gfx) const noxnd
+	{
+		pDrawable->Bind(gfx);
+		pStep->Bind(gfx);
+		gfx.DrawIndexed(pDrawable->GetIndexCount());
+	}
 }
